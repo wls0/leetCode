dfbaf41454945f9b88d414b5b2ba6f03b53b3a32
wls0 <50974991+wls0@users.noreply.github.com>
2025-02-21 00:59:12 +0900

    Time: 6 ms (6.69%) | Memory: 58.7 MB (5.06%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.ts
@@ -0,0 +1,30 @@
+/**
+ Do not return anything, modify nums1 in-place instead.
+ */
+function merge(nums1: number[], m: number, nums2: number[], n: number) {
+	let data: number[] = [];
+    let nums2Data = [];
+
+	data = [...nums1.slice(0, m)]
+	data.filter((el) => el !== 0);
+	
+	nums2Data = [...nums2.slice(0, n)]
+    nums2Data.filter((el)=> el!==0);
+
+    data = [...data, ...nums2Data];
+	data.sort((a, b) => {
+		if (a < b) {
+			return -1;
+		} else {
+			return 1;
+		}
+	});
+
+    console.log(data)
+    nums1.splice(0, nums1.length);
+    for(const c of data){
+        nums1.push(c)
+    }
+    
+
+}
