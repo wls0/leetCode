97d53d19c2bc73de03f28eac1b34f3cbf9a17b4b
wls0 <50974991+wls0@users.noreply.github.com>
2025-03-04 14:41:08 +0900

    Time: 3 ms (54.73%) | Memory: 61.3 MB (5.10%) - LeetSync

--- 136-single-number/single-number.ts
+++ 136-single-number/single-number.ts
@@ -1,12 +1,22 @@
 function singleNumber(nums: number[]): number {
-    const set = [...new Set(nums)];
-    const numLength = nums.length;
-    for(const a of set){
-        const filter = nums.filter((e)=> a !== e );
-        if(numLength -1 === filter.length){
-            return a;
+    // const set = [...new Set(nums)];
+    // const numLength = nums.length;
+    // for(const a of set){
+    //     const filter = nums.filter((e)=> a !== e );
+    //     if(numLength -1 === filter.length){
+    //         return a;
+    //     }
+    // }
+
+    const set = new Set<number>();
+
+    for(const a of nums){
+        if(set.has(a)){
+            set.delete(a)
+        }else{
+            set.add(a)
         }
     }
-
-
+    const result = [...set];
+    return result[0]
 };
